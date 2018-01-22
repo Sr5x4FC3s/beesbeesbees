f73711d2cc2e435fb689e64ec0f48f145aec7427
Sr5x4FC3s <32333361+Sr5x4FC3s@users.noreply.github.com>
2018-01-22 14:25:19 -0800

    finished

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,13 @@
 var Bee = function() {
+  Grub.call(this);
+  this.age = 5;
+  this.color = 'yellow';
+  // this.food = 'jelly';
+  // Grub.prototype.eat.call(this);
+  // this.eat = Grub.prototype.eat;
+  this.job = 'keep on growing';
 };
-
+Bee.prototype = Object.create(Grub.prototype);
+Bee.prototype.constructor = Bee;
+// Bee.prototype.job = 'keep on growing';
+// var bee = new Bee();
