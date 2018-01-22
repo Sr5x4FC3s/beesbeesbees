923130295e3a00cbcc37560e1bf561d5f54240f3
Sr5x4FC3s <32333361+Sr5x4FC3s@users.noreply.github.com>
2018-01-22 12:46:45 -0800

    finished grub

--- src/Grub.js
+++ src/Grub.js
@@ -1,3 +1,36 @@
+//things that grub need: age set to 0, color set to pink, food set to
+//jelly, eat method; 
+
 var Grub = function() {
+  //grub = {};
+  this.age;
+  this.color;
+  this.food;
+  this.eat;
 };
 
+// Grub.prototype.age = function(age) {
+//   // this.grub.age = 0;
+//   // return this.grub.age = 0;
+//   return this.age = 0;
+// };
+
+Grub.prototype.age = 0;
+
+// Grub.prototype.color = function() {
+//   return this.color = 'pink';
+// };
+
+Grub.prototype.color = 'pink';
+
+// Grub.prototype.food = function() {
+//   this.food = 'jelly';
+// };
+
+Grub.prototype.food = 'jelly';
+
+Grub.prototype.eat = function() {
+  return this.eat;
+};
+
+var grub = new Grub();
